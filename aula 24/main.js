f5adec135e5c7096babe9fa28b538e60c055014c
Vivianeeee <vivianeoliveirandrade@gmail.com>
2021-11-04 21:53:50 -0300

    condicionais

---
+++ aula 24/main.js
@@ -0,0 +1,38 @@
+const a = 10;
+if (a == 10) {
+   console.log("os valores são iguais");
+} else if (a > 10) {
+   console.log("A é maior que 10");
+} else {
+   console.log("A variavel A não é igual a 10 e não é maior que 10")
+}
+
+// SWITCH
+const nota = 9;
+
+switch (nota) {
+      case 4:
+      console.log("Reprovado!");
+      break;
+      case 6:
+      console.log("Recuperação");
+      break;
+      default:
+         console.log("Nota acima da média");
+         break;
+}
+console.clear();
+const b = 10;
+if (b== 10) {
+   console.log("os valores são iguais");
+}else {
+   console.log("os valores são diferentes");
+}
+
+
+let c = b == 16? "é igual a 10" : "são diferentes";
+console.log(c);
+
+let resultado= 3 != 4 ? "3 é diferente de 4" : "3 é igual a 4";
+
+
